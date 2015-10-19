b751047378a7b0e24f46d63f1371f7762c7f6c46
unknown <shaul@bacdev10.bacsoft.local>
2015-10-19 16:40:36 +0300

    Fixed version

--- ext_info.py
+++ ext_info.py
@@ -20,10 +20,11 @@ folder = Path(sys.argv[1])
 for p in folder.iterdir():
 	if (p.is_file()):
 		fileName = os.path.splitext(str(p))[1][1:]
-		if (fileName == ''):
-			fileName = '.'
 		files[fileName]['counter'] += 1
 		files[fileName]['size'] += p.stat().st_size
+
+files['.'] = files['']
+del files['']
 
 for name, info in files.items():
     print(name, info['counter'], info['size'])
